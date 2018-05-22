80da125988383d55fd0f83ad066df80e3fd754b7
Elizabeth Zarela Zanabria Chuquipiondo <zarelixzana@gmail.com>
2018-05-21 21:56:16 -0500

    Creando los metodos de Cifrar y descifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,17 @@
 window.cipher = {
-  // ... 
+
+  //Creando el metodo de Cifrado
+
+
+  cipher.encode(offset,string){
+
+  }
+
+  cipher.decode(offset, string){
+
+  }
+
+
+  // creando el metodo de Descifrar
+
 };
