97f20e39205a9134c333a2cbe97e087cd40d4e08
ZarelixCoder <zarelixzana@gmail.com>
2018-05-31 11:15:38 -0500

    Realizando el Testing de Cifrado y Descifrado con Mayus, Minus, espacio

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -13,6 +13,13 @@ describe('cipher', () => {
 
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33',()=>{
       assert.equal(cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),'HIJKLMNOPQRSTUVWXYZABCDEFG');
+    });
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFGhijklmnopqrstuvwxyzabdefg" para "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz" con offest 33',()=>{
+      assert.equal(cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz'),'HIJKLMNOPQRSTUVWXYZABCDEFGhijklmnopqrstuvwxyzabdefg');
+    })
+    it('debería retornar "H I J KLM NOP QRSTU VWXYZABCDEFGhij klmnop qrstuvwxyza bdefg" para "A B C DEF GHI JKLMN OPQRSTUVWXYZabc defghi jklmnopqrst uwxyz" con offest 33',()=>{
+      assert.equal(cipher.encode(33,'A B C DEF GHI JKLMN OPQRSTUVWXYZabc defghi jklmnopqrst uwxyz'),'H I J KLM NOP QRSTU VWXYZABCDEFGhij klmnop qrstuvwxyza bdefg');
     })
   });
 
@@ -25,6 +32,12 @@ describe('cipher', () => {
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33',()=>{
       assert.equal(cipher.decode(33,'HIJKLMNOPQRSTUVWXYZABCDEFG'),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
     });
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz" para "HIJKLMNOPQRSTUVWXYZABCDEFGhijklmnopqrstuvwxyzabdefg" con offest 33',()=>{
+      assert.equal(cipher.decode(33,'HIJKLMNOPQRSTUVWXYZABCDEFGhijklmnopqrstuvwxyzabdefg'),'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz');
+    });
+    it('debería retornar "A B C DEF GHI JKLMN OPQRSTUVWXYZabc defghi jklmnopqrst uwxyz" para "H I J KLM NOP QRSTU VWXYZABCDEFGhij klmnop qrstuvwxyza bdefg" con offest 33',()=>{
+      assert.equal(cipher.decode(33,'H I J KLM NOP QRSTU VWXYZABCDEFGhij klmnop qrstuvwxyza bdefg'),'A B C DEF GHI JKLMN OPQRSTUVWXYZabc defghi jklmnopqrst uwxyz');
+    });
   });
 
   describe('cipher.createCipherWithOffset', () => {
@@ -36,6 +49,7 @@ describe('cipher', () => {
     it('debería retornar un objeto con dos funciones (encode y decode) con offset fijado',()=>{
             
       assert.isObject( cipher.createCipherWithOffset(3) , 'Si es un objeto');
+      assert.property(cipher.createCipherWithOffset(33), cipher.createCipherWithOffset().encode(),'Tiene la proiedad encode' );
       assert.equal(typeof cipher.createCipherWithOffset(33).encode('A'),'function');
       assert.equal(typeof cipher.createCipherWithOffset(33).decode('H'),'function');
 
